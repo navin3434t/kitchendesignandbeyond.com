70923e33f86988703fde304cac3fe660f5587671
mac <mac@macs-MacBook-Pro.local>
2020-07-28 00:50:32 +0530

    Checking

--- wp-content/themes/fona-child/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/fona-child/woocommerce/single-product/add-to-cart/simple.php
@@ -46,7 +46,7 @@ if ( $product->is_in_stock() ) : ?>
 		do_action( 'woocommerce_after_add_to_cart_quantity' );
 		?>
 
-		<input type="submit" name="add-to-cart" data-product_id="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt" value="<?php echo esc_html( $product->single_add_to_cart_text() ); ?>"/>
+		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
