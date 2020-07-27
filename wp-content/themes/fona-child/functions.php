322349dc2d81e5d9ba9d6e703baaba43d9de3ccb
mac <mac@macs-MacBook-Pro.local>
2020-07-28 00:15:29 +0530

    Changes

--- wp-content/themes/fona-child/functions.php
+++ wp-content/themes/fona-child/functions.php
@@ -4,7 +4,12 @@
 	 * test comment
 	 * @link https://developer.wordpress.org/themes/basics/theme-functions/
 	 */
-
+	function mytheme_add_woocommerce_support() {
+		add_theme_support( 'woocommerce' );
+	}
+	
+	add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+	
 	function cfwc_create_custom_field() {
 		$args = array(
 		'id' => 'box_pieces',
